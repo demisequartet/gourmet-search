51e8c067ccf4390ef81fc19aeaa0c0a955487968
demisequartet <kaizukasi551@gmail.com>
2023-03-31 23:29:05 +0900

    firstcommit

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,23 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
+//axiosをimport
+import axios from "axios";
+
 export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
+  const url = "http://webservice.recruit.co.jp/hotpepper/gourmet/v1/";
+  const apikey = "014d83b70fa0a10e";
+
+  const queryString = `?key=${apikey}&format=json&large_area=Z011&keyword=肉&count=10`;
+
+  axios
+    .get(url + queryString)
+    .then(function (response) {
+      // handle success
+
+      res.status(200).json(response.data);
+    })
+    .catch(function (error) {
+      // handle error
+      console.log(error);
+    });
 }
