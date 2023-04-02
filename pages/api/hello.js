c1fa0f8c9b9a113c91e616bd3c3543c18e9a774d
demisequartet <kaizukasi551@gmail.com>
2023-04-03 00:09:39 +0900

    API route関連

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -4,16 +4,20 @@
 import axios from "axios";
 
 export default function handler(req, res) {
-  const url = "http://webservice.recruit.co.jp/hotpepper/gourmet/v1/";
+  const url = "http://webservice.recruit.co.jp/hotpepper/gourmet/v1/?";
   const apikey = "014d83b70fa0a10e";
 
-  const queryString = `?key=${apikey}&format=json&large_area=Z011&keyword=肉&count=10`;
+  // const queryString = `?key=${apikey}&format=json&large_area=Z011&keyword=肉&count=10`;
+
+  const queryString = new URLSearchParams(req.query);
+
+  console.log(req.query);
 
   axios
     .get(url + queryString)
     .then(function (response) {
       // handle success
-
+      console.log("success");
       res.status(200).json(response.data);
     })
     .catch(function (error) {
