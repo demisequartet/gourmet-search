e3ad7ef3ed7eeaa1303c86b6e45eb89d170548cc
demisequartet <kaizukasi551@gmail.com>
2023-04-05 00:55:01 +0900

    検索結果画面実装（仮）

--- pages/components/SearchResult.jsx
+++ pages/components/SearchResult.jsx
@@ -1,42 +1,198 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import { useRouter } from "next/router";
+import Link from "next/link";
+import Image from "next/image";
+
+//表示関連
+import PropTypes from "prop-types";
+import { useTheme } from "@mui/material/styles";
+import Box from "@mui/material/Box";
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableHead from "@mui/material/TableHead";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableFooter from "@mui/material/TableFooter";
+import TablePagination from "@mui/material/TablePagination";
+import TableRow from "@mui/material/TableRow";
+import Paper from "@mui/material/Paper";
+import IconButton from "@mui/material/IconButton";
+import FirstPageIcon from "@mui/icons-material/FirstPage";
+import KeyboardArrowLeft from "@mui/icons-material/KeyboardArrowLeft";
+import KeyboardArrowRight from "@mui/icons-material/KeyboardArrowRight";
+import LastPageIcon from "@mui/icons-material/LastPage";
+
+function TablePaginationActions(props) {
+  const theme = useTheme();
+  const { count, page, rowsPerPage, onPageChange } = props;
+
+  const handleFirstPageButtonClick = (event) => {
+    onPageChange(event, 0);
+  };
+
+  const handleBackButtonClick = (event) => {
+    onPageChange(event, page - 1);
+  };
+
+  const handleNextButtonClick = (event) => {
+    onPageChange(event, page + 1);
+  };
+
+  const handleLastPageButtonClick = (event) => {
+    onPageChange(event, Math.max(0, Math.ceil(count / rowsPerPage) - 1));
+  };
+
+  return (
+    <Box sx={{ flexShrink: 0, ml: 2.5 }}>
+      <IconButton
+        onClick={handleFirstPageButtonClick}
+        disabled={page === 0}
+        aria-label="first page"
+      >
+        {theme.direction === "rtl" ? <LastPageIcon /> : <FirstPageIcon />}
+      </IconButton>
+      <IconButton onClick={handleBackButtonClick} disabled={page === 0} aria-label="previous page">
+        {theme.direction === "rtl" ? <KeyboardArrowRight /> : <KeyboardArrowLeft />}
+      </IconButton>
+      <IconButton
+        onClick={handleNextButtonClick}
+        disabled={page >= Math.ceil(count / rowsPerPage) - 1}
+        aria-label="next page"
+      >
+        {theme.direction === "rtl" ? <KeyboardArrowLeft /> : <KeyboardArrowRight />}
+      </IconButton>
+      <IconButton
+        onClick={handleLastPageButtonClick}
+        disabled={page >= Math.ceil(count / rowsPerPage) - 1}
+        aria-label="last page"
+      >
+        {theme.direction === "rtl" ? <FirstPageIcon /> : <LastPageIcon />}
+      </IconButton>
+    </Box>
+  );
+}
+
+TablePaginationActions.propTypes = {
+  count: PropTypes.number.isRequired,
+  onPageChange: PropTypes.func.isRequired,
+  page: PropTypes.number.isRequired,
+  rowsPerPage: PropTypes.number.isRequired,
+};
 
 function SearchResult() {
   const router = useRouter();
-  const data = JSON.parse(router.query.formData);
-
-  const [latitude, setLatitude] = useState(0.0);
-  const [longitude, setLongitude] = useState(0.0);
+  const data = router.query;
   const [stores, setStores] = useState([]);
 
   useEffect(() => {
-    //位置情報取得
-    navigator.geolocation.getCurrentPosition((position) => {
-      console.log(`latitude: ${position.coords.latitude} longitude: ${position.coords.longitude}`);
-      setLatitude(position.coords.latitude);
-      setLongitude(position.coords.longitude);
-    });
-
     const apikey = "014d83b70fa0a10e";
-    const query = `?key=${apikey}&format=json&lat=${latitude}&lng=${longitude}&range=${data.number}&count=100`;
-    const url = "/api/hello/" + query;
 
-    axios
-      .get(url)
-      .then(function (response) {
-        // handle success
-        setStores(response.data.results.shop);
-        console.log("stores");
-        console.log(response.data.results.shop);
-      })
-      .catch(function (error) {
-        // handle error
-        console.log(error);
-      });
-  }, [latitude, longitude, data.number]);
+    // 位置情報取得
+    navigator.geolocation.getCurrentPosition((position) => {
+      // console.log(`latitude: ${position.coords.latitude} longitude: ${position.coords.longitude}`);
+      const query = `?key=${apikey}&format=json&lat=${position.coords.latitude}&lng=${position.coords.longitude}&range=${data.number}&count=100`;
+      const url = "/api/hello/" + query;
 
-  return <div>aaaaaaaa</div>;
+      axios
+        .get(url)
+        .then(function (response) {
+          // handle success
+          setStores(response.data.results.shop);
+          console.log("stores");
+          console.log(response.data.results.shop);
+        })
+        .catch(function (error) {
+          // handle error
+          console.log(error);
+        });
+    });
+  }, []);
+
+  const [page, setPage] = React.useState(0);
+  const [rowsPerPage, setRowsPerPage] = React.useState(5);
+
+  // Avoid a layout jump when reaching the last page with empty rows.
+  const emptyRows = page > 0 ? Math.max(0, (1 + page) * rowsPerPage - stores.length) : 0;
+
+  const handleChangePage = (event, newPage) => {
+    setPage(newPage);
+  };
+
+  const handleChangeRowsPerPage = (event) => {
+    setRowsPerPage(parseInt(event.target.value, 10));
+    setPage(0);
+  };
+
+  return (
+    <>
+      <h1>検索結果</h1>
+      <TableContainer component={Paper}>
+        <Table sx={{ minWidth: 500 }} aria-label="custom pagination table">
+          <TableHead>
+            <TableRow>
+              <TableCell>店名</TableCell>
+              <TableCell>アクセス</TableCell>
+              <TableCell>サムネイル</TableCell>
+              <TableCell>詳細ページへ</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {(rowsPerPage > 0
+              ? stores.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+              : stores
+            ).map((store) => (
+              <TableRow key={store.name}>
+                <TableCell component="tr" scope="row">
+                  {store.name}
+                </TableCell>
+                <TableCell component="tr" scope="row">
+                  {store.access}
+                </TableCell>
+                <TableCell component="tr" scope="row">
+                  <img src={store.photo.pc.m} />
+                </TableCell>
+                <TableCell component="tr" scope="row">
+                  <Link
+                    href={{ pathname: "/components/StoreInfo", query: store }}
+                    as="/components/StoreInfo"
+                  >
+                    詳細ページへ
+                  </Link>
+                </TableCell>
+              </TableRow>
+            ))}
+
+            {emptyRows > 0 && (
+              <TableRow style={{ height: 53 * emptyRows }}>
+                <TableCell colSpan={6} />
+              </TableRow>
+            )}
+          </TableBody>
+          <TableFooter>
+            <TableRow>
+              <TablePagination
+                rowsPerPageOptions={[5, 10, 25, { label: "All", value: -1 }]}
+                colSpan={3}
+                count={stores.length}
+                rowsPerPage={rowsPerPage}
+                page={page}
+                SelectProps={{
+                  inputProps: {
+                    "aria-label": "rows per page",
+                  },
+                  native: true,
+                }}
+                onPageChange={handleChangePage}
+                onRowsPerPageChange={handleChangeRowsPerPage}
+                ActionsComponent={TablePaginationActions}
+              />
+            </TableRow>
+          </TableFooter>
+        </Table>
+      </TableContainer>
+    </>
+  );
 }
 
 export default SearchResult;
